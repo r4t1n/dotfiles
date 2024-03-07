c74eb384c0fb1ce7030ff65684d6ad83a79ac035
r4t1n <r4t1n@proton.me>
2024-03-07 14:34:07 +0100

    update configs and readme.md

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Install GNU stow from your package manager
 
 ```
-cd && git clone https://github.com/r4t1n/dotfiles.git && cd dotfiles
+cd ~ && git clone https://github.com/r4t1n/dotfiles && cd dotfiles
 ```
 
 ```
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,19 @@
 if status is-interactive
-	# Disable fish greeting
-	function fish_greeting
-
-	end
-
 	# Initialize shell stuff
 	starship init fish | source
 	zoxide init --cmd cd fish | source
 
 	# Set aliases
-	alias aocd="aoc --input-only d"
 	alias cat="bat"
 	alias crg="cargo fmt && cargo check && cargo clippy && cargo build --release"
 	alias ls="exa"
 	alias off="shutdown -h now"
-	alias plasma="startplasma-wayland"
 	
 	# Set environment variables
 	set -x EDITOR vim
+
+	# Disable fish greeting
+	function fish_greeting
+	
+	end
 end
