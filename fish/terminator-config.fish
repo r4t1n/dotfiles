268c7249c2d6747c5e552602d629afc1800a94d4
r4t1n <r4t1n@proton.me>
2024-03-29 15:06:21 +0100

    add yeet-orphans to fish terminator

--- fish/terminator-config.fish
+++ fish/terminator-config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
 	alias upd="doas emerge --update --deep --newuse @world; curl -d 'Update complete' 10.2/commands"
+	alias yeet-orphans"doas emerge --depclean"
 
 	function sudo!!
 		eval sudo $history[1]
