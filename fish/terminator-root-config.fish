76d71c1208113cf6745e87b3a3a6cdac4124a824
r4t1n <r4t1n@proton.me>
2024-03-30 16:53:25 +0100

    oops

--- fish/terminator-root-config.fish
+++ fish/terminator-root-config.fish
@@ -1,3 +1,3 @@
 if status is-interactive
-	alias srclinux="make -j2 -l2; make modules_install install; rm /boot/*.old /boot/grub/grub.cfg~; curl -d 'Linux install complete' 10.2/commands"
+	alias srclinux="make -j2 -l2; make modules_install install; rm /boot/*.old /boot/grub/grub.cfg~; curl -d '$(cat /etc/hostname): Linux install complete' 10.2/updates"
 end
